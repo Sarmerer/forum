56488e046e4a3fa66ff46ed6677661bf166b1b38
sarmerer <sarmerer@gmail.com>
2020-08-16 17:01:22 +0500

    removed node_modules

--- api/controllers/home.go
+++ api/controllers/home.go
@@ -2,15 +2,8 @@ package controllers
 
 import (
 	"net/http"
-	"text/template"
 )
 
 func GetHome(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseGlob("./ui/templates/*html"))
-	data := struct {
-		Title string
-		Next  string
-	}{"Ban", "test"}
-	tmpl.ExecuteTemplate(w, "base.html", data)
-	//w.Write([]byte("get home"))
+	w.Write([]byte("get home"))
 }
