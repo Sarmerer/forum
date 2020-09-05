7fb13346a3edceaa2d0e71aa43d2610cab0e949b
sarmai <ailensarmukhanova@gmail.com>
2020-09-05 13:29:32 +0500

    deleted all and created new app

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
---
+++ ui/src/app/nav/nav.component.html
@@ -0,0 +1,6 @@
+<ul>
+    <li><a href="#home">Home</a></li>
+    <li><a href="#news">News</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li style="float:right"><a class="active" href="#about">About</a></li>
+</ul>
