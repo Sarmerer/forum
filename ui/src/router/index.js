cb1ec26b5991e84bd19d8819a01916c06c05769e
sarmerer <sarmerer@gmail.com>
2020-11-16 15:02:43 +0500

    added basic middleware

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,14 +1,18 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "@/views/Home.vue";
+import store from "@/store";
 
 Vue.use(VueRouter);
+
+const ROLES = {};
+ROLES.ADMIN = 2;
 
 const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
   },
   {
     path: "/post/:id",
@@ -16,19 +20,31 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "post" */ "@/views/Post.vue")
+    component: () => import(/* webpackChunkName: "post" */ "@/views/Post.vue"),
   },
   {
     path: "/user/:id",
     name: "User",
-    component: () => import(/* webpackChunkName: "user" */ "@/views/User.vue")
-  }
+    component: () => import(/* webpackChunkName: "user" */ "@/views/User.vue"),
+  },
+  {
+    path: "/dashboard/:role",
+    name: "Admin Dashnoard",
+    component: () =>
+      import(/* webpackChunkName: "user" */ "@/views/Dashboard.vue"),
+    beforeEnter: (to, from, next) => {
+      let user = store.getters["auth/user"];
+      !store.getters["auth/authenticated"] || !user || user.role < ROLES.ADMIN
+        ? next("/")
+        : next();
+    },
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
