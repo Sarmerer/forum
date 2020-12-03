2455f7c3478564f4ee65ed9e96564472089ed746
sarmerer <sarmerer@gmail.com>
2020-12-03 22:21:40 +0500

    reworked user card

--- ui/src/components/UserCard.vue
+++ ui/src/components/UserCard.vue
@@ -1,121 +1,77 @@
 <template>
   <div :class="isMobile() ? 'card-m' : 'card'">
-    <div v-if="!isMobile()">
-      <b-row>
-        <b-col align="center">
-          <b-img-lazy
-            v-if="userData.avatar"
-            :src="userData.avatar"
-            alt="avatar"
-            rounded="circle"
-            class="mb-1"
-            width="100px"
-          >
-          </b-img-lazy>
-          <router-link v-if="link" :to="`/user/${userData.id}`">
-            <h3 class="primary">
-              {{ userData.display_name }}
-              <b-badge v-if="userData.role == 2" class="background-variant">
-                <b-icon-code-slash></b-icon-code-slash>
-              </b-badge>
-            </h3>
-          </router-link>
-          <h3 v-else class="primary">
+    <b-row>
+      <b-col cols="start">
+        <b-avatar
+          v-if="userData.avatar"
+          :src="userData.avatar"
+          alt="avatar"
+          size="lg"
+          class="ml-3"
+          variant="dark"
+          badge-offset="-0.2em"
+          :badge="userData.role > 0"
+          :badge-right="userData.role > 0"
+          badge-variant="info"
+        >
+          <template #badge><b-icon-code-slash></b-icon-code-slash></template>
+        </b-avatar>
+      </b-col>
+      <b-col>
+        <router-link v-if="link" :to="`/user/${userData.id}`">
+          <h3 class="primary">
             {{ userData.display_name }}
-            <b-badge v-if="userData.role == 2" class="background-variant">
-              <b-icon-code-slash></b-icon-code-slash>
-            </b-badge>
           </h3>
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col>
-          <p class="m-0 px-2">
-            Joined:
-            <time-ago
-              v-if="userData.created"
-              v-b-tooltip.hover
-              :title="userData.created"
-              :datetime="userData.created"
-              :long="!isMobile()"
-            >
-            </time-ago>
-            <br />
-            Active:
-            <time-ago
-              v-if="userData.last_active"
-              v-b-tooltip.hover
-              :title="userData.last_active"
-              :datetime="userData.last_active"
-              :long="!isMobile()"
-            >
-            </time-ago>
-            <br />
-            Rating: {{ userData.rating }}<br />
-            Posts: {{ userData.posts }}<br />
-            Comments: {{ userData.comments }}<br />
-          </p>
-        </b-col>
-      </b-row>
-    </div>
-
-    <div v-else>
-      <b-row align-v="center">
-        <b-col align="center">
-          <router-link v-if="link" :to="`/user/${userData.id}`">
-            <b-img-lazy
-              v-if="userData.avatar"
-              :src="userData.avatar"
-              alt="avatar"
-              rounded="circle"
-              class="mb-1"
-              width="70px"
-            >
-            </b-img-lazy>
-            <h4 class="primary m-0">
-              {{ userData.display_name }}
-              <b-badge v-if="userData.role == 2" class="background-variant">
-                <b-icon-code-slash></b-icon-code-slash>
-              </b-badge>
-            </h4>
-          </router-link>
-
-          <div v-else>
-            <b-img-lazy
-              v-if="userData.avatar"
-              :src="userData.avatar"
-              alt="avatar"
-              rounded="circle"
-              class="mb-1"
-              width="70px"
-            >
-            </b-img-lazy>
-            <h4 class="primary m-0">
-              {{ userData.display_name }}
-              <b-badge v-if="userData.role == 2" class="background-variant">
-                <b-icon-code-slash></b-icon-code-slash>
-              </b-badge>
-            </h4>
-          </div>
-        </b-col>
-        <b-col>
-          <p class="m-0">
-            Active:
-            <time-ago
-              v-b-tooltip.hover
-              :title="userData.last_active"
-              :datetime="userData.last_active"
-              :long="!isMobile()"
-            >
-            </time-ago>
-            <br />
-            Rating: {{ userData.rating }}<br />
-            Posts: {{ userData.posts }}<br />
-            Comments: {{ userData.comments }}<br />
-          </p>
-        </b-col>
-      </b-row>
-    </div>
+        </router-link>
+        <h3 v-else class="primary mb-0">
+          {{ userData.display_name }}
+        </h3>
+        <small>
+          Active:
+          <time-ago
+            v-if="userData.last_active"
+            v-b-tooltip.hover
+            :title="userData.last_active"
+            :datetime="userData.last_active"
+            :long="!isMobile()"
+          >
+          </time-ago>
+        </small>
+      </b-col>
+    </b-row>
+    <b-row class="ml-2 mt-2">
+      <b-col>
+        <div v-b-tooltip.hover.left title="Joined">
+          <b-icon-calendar-event></b-icon-calendar-event>
+        </div>
+        <time-ago
+          v-if="userData.created"
+          v-b-tooltip.hover.bottom
+          :title="userData.created"
+          :datetime="userData.created"
+        >
+        </time-ago>
+      </b-col>
+      <b-col>
+        <div v-b-tooltip.hover.bottom title="Rating">
+          <b-icon :icon="userData.rating >= 0 ? 'arrow-up' : 'arrow-down'">
+          </b-icon>
+        </div>
+        {{ userData.rating }}
+      </b-col>
+      <b-col>
+        <div v-b-tooltip.hover.bottom title="Posts">
+          <b-icon-newspaper></b-icon-newspaper>
+        </div>
+        {{ userData.posts }}
+      </b-col>
+      <b-col>
+        <div v-b-tooltip.hover.right title="Comments">
+          <b-icon-chat></b-icon-chat>
+        </div>
+        {{ userData.comments }}
+      </b-col>
+    </b-row>
   </div>
 </template>
 <script>
@@ -123,7 +79,7 @@ import TimeAgo from "vue2-timeago";
 
 export default {
   name: "user-card",
-  props: { userData: { type: Object }, link: { type: Boolean } },
+  props: { userData: Object, link: Boolean },
   components: {
     TimeAgo,
   },
